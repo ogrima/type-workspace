d3ec48143e15a9642e66101e110953839a9864ab
ogrima <lbgrimaldi@gmail.com>
2018-08-05 12:06:50 -0300

    ajustes no exemplo.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var myString = "Testando meu texto";
+var myNumber = 1225;
+/*teste*/
+function log(msg) {
+    console.log(msg);
+}
+log(myString + " - " + myNumber.toString());
+log("Foi!!!");
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+let myString: String = "Testando meu texto";
+let myNumber: number = 1225;
+
+
+/*teste*/
+function log(msg: String){
+    console.log(msg);
+}
+
+
+log(myString + " - " + myNumber.toString());
+log("Foi!!!");
+
